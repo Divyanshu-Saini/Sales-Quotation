db6292a083d62600f2844747f490e6fcea8a662b
Divyanshu Saini <Divyanshu-Saini@users.noreply.github.com>
2017-11-01 14:56:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const apiai = require('apiai');
 
-
 //Access keys
 const APIAI_ACCESS_KEY = '2c9694a9e7f345538c7c0feeecc90cc6';
 
@@ -17,7 +16,6 @@ const spiaiApp = apiai(APIAI_ACCESS_KEY);
 //Configuring port and other settings
 app.set('PORT', (process.env.PORT || 2828));
 
-
 //Using Middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -27,15 +25,14 @@ app.set("view options", {layout: false});
 app.use(express.static(__dirname + '/public'));
 
 //Webhook for openweather api
-app.post('/sales-quotation', (req, res) => {
-    
+app.post('/sales-quotation', (req, res) => {   
 });
 
 /*app.get('/', function (req, res) {
 res.sendfile(__dirname + '/public/home.html');
 });*/
-
 //Starting server
+
 const server = app.listen(app.get('PORT'), function () {
     console.log("Express server listening on port %d in %s mode", server.address().port, app.settings.env);
 });
